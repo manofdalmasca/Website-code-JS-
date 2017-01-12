b4916be4b9fc90ee89405ad32eca4a62f609c356
manofdalmasca <redstern13@yahoo.com>
2017-01-12 18:54:18 -0500

    Version 2.0, parts 1 - 3 (2016 - present)
    Javascript code for version 2.0 of the site's main page, as well as /picDir and /runtimes. Allows the user to customize the site's colors. Visible since June 2016 at [www,samuel-stern.info].

---
+++ kecleonabout.js
@@ -0,0 +1,41 @@
+function redize() {
+	document.getElementById("tudo").style.backgroundColor = "#ffb3b3"; 
+	document.getElementById("headr").style.backgroundColor = "#ff8080"; 
+	document.getElementById("bods").style.backgroundColor = "#ff8080"; 
+	document.getElementById("footr").style.backgroundColor = "#ff8080"; 
+}
+
+function yellowize() {
+	document.getElementById("tudo").style.backgroundColor = "#ffff99"; 
+	document.getElementById("headr").style.backgroundColor = "#ffcc00"; 
+	document.getElementById("bods").style.backgroundColor = "#ffcc00"; 
+	document.getElementById("footr").style.backgroundColor = "#ffcc00"; 
+}
+
+function greenize() {
+	document.getElementById("tudo").style.backgroundColor = "#66ffcc"; 
+	document.getElementById("headr").style.backgroundColor = "#00e68a"; 
+	document.getElementById("bods").style.backgroundColor = "#00e68a"; 
+	document.getElementById("footr").style.backgroundColor = "#00e68a"; 
+}
+
+function blueize() {
+	document.getElementById("tudo").style.backgroundColor = "#80ccff"; 
+	document.getElementById("headr").style.backgroundColor = "#1aa3ff"; 
+	document.getElementById("bods").style.backgroundColor = "#1aa3ff"; 
+	document.getElementById("footr").style.backgroundColor = "#1aa3ff"; 
+}
+
+function violetize() {
+	document.getElementById("tudo").style.backgroundColor = "#ffb3ff"; 
+	document.getElementById("headr").style.backgroundColor = "#ff66ff"; 
+	document.getElementById("bods").style.backgroundColor = "#ff66ff"; 
+	document.getElementById("footr").style.backgroundColor = "#ff66ff"; 
+}
+
+function silverize() {
+	document.getElementById("tudo").style.backgroundColor = "#f2f2f2"; 
+	document.getElementById("headr").style.backgroundColor = "#d9d9d9"; 
+	document.getElementById("bods").style.backgroundColor = "#d9d9d9"; 
+	document.getElementById("footr").style.backgroundColor = "#d9d9d9"; 
+}
---
+++ kecleonrun.js
@@ -0,0 +1,41 @@
+function redize() {
+	document.getElementById("inteiro").style.backgroundColor = "#ffb3b3"; 
+	document.getElementById("headr").style.backgroundColor = "#ff8080"; 
+	document.getElementById("bods").style.backgroundColor = "#ff8080"; 
+	document.getElementById("footr").style.backgroundColor = "#ff8080";  
+}
+
+function yellowize() {
+	document.getElementById("inteiro").style.backgroundColor = "#ffff99"; 
+	document.getElementById("headr").style.backgroundColor = "#ffcc00"; 
+	document.getElementById("bods").style.backgroundColor = "#ffcc00"; 
+	document.getElementById("footr").style.backgroundColor = "#ffcc00";  
+}
+
+function greenize() {
+	document.getElementById("inteiro").style.backgroundColor = "#66ffcc"; 
+	document.getElementById("headr").style.backgroundColor = "#00e68a"; 
+	document.getElementById("bods").style.backgroundColor = "#00e68a"; 
+	document.getElementById("footr").style.backgroundColor = "#00e68a"; 
+}
+
+function blueize() {
+	document.getElementById("inteiro").style.backgroundColor = "#80ccff"; 
+	document.getElementById("headr").style.backgroundColor = "#1aa3ff"; 
+	document.getElementById("bods").style.backgroundColor = "#1aa3ff"; 
+	document.getElementById("footr").style.backgroundColor = "#1aa3ff"; 
+}
+
+function violetize() {
+	document.getElementById("inteiro").style.backgroundColor = "#ffb3ff"; 
+	document.getElementById("headr").style.backgroundColor = "#ff66ff"; 
+	document.getElementById("bods").style.backgroundColor = "#ff66ff"; 
+	document.getElementById("footr").style.backgroundColor = "#ff66ff"; 
+}
+
+function silverize() {
+	document.getElementById("inteiro").style.backgroundColor = "#f2f2f2"; 
+	document.getElementById("headr").style.backgroundColor = "#d9d9d9"; 
+	document.getElementById("bods").style.backgroundColor = "#d9d9d9"; 
+	document.getElementById("footr").style.backgroundColor = "#d9d9d9";  
+}
